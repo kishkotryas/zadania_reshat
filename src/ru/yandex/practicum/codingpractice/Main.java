d17dcb76e368152064af4b2ee8ffa008d4270f73
kishkotryas <scrath.thecountry.02@mail.ru>
2025-12-17 01:58:37 +0500

    Задание на переопределение equals и hashCode

--- src/ru/yandex/practicum/codingpractice/Main.java
+++ src/ru/yandex/practicum/codingpractice/Main.java
@@ -2,8 +2,11 @@ package ru.yandex.practicum.codingpractice;
 
 public class Main {
     public static void main (String [] args) {
-        ScientificCalculator scientificCalculator = new ScientificCalculator();
-        System.out.println(scientificCalculator.power(2, 9));
+        Student student1 = new Student("1001", "Павел", 25);
+        Student student2 = new Student("1001", "Игорь", 11);
+
+        System.out.println("Хэш код первого объекта: " + student1.hashCode());
+        System.out.println("Хэш код второго объекта: " + student2.hashCode());
     }
 }
 
