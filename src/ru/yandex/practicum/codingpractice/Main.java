ac002bace80ff12c9e81b863cf739a09336110d7
kishkotryas <scrath.thecountry.02@mail.ru>
2026-01-05 22:47:58 +0500

    feat: задание на сортировку вставкой

--- src/ru/yandex/practicum/codingpractice/Main.java
+++ src/ru/yandex/practicum/codingpractice/Main.java
@@ -2,34 +2,17 @@ package ru.yandex.practicum.codingpractice;
 
 public class Main {
     public static void main (String [] args) {
-        Animal[] array =
-                {
-                        new Cat("Шуля", 2),
-                        new Cat("Евгеша", 4),
-                        new Dog("Барон", 3),
-                        new Dog("Пират", 7),
-                        new Parrot("Ларри", 1)
-                };
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] instanceof Mammal) {
-                Mammal mammal = (Mammal) array[i];
-                mammal.produceMilk();
-            }
-            if (array[i] instanceof Bird) {
-                Bird bird = (Bird) array[i];
-                bird.fly();
-            }
-            if (array[i] instanceof Dog) {
-                Dog dog = (Dog) array[i];
-                dog.bark();
-            }
-            if (array[i] instanceof Cat) {
-                Cat cat = (Cat) array[i];
-                cat.meow();
-            }
-            if (array[i] instanceof Parrot) {
-                Parrot parrot = (Parrot) array[i];
-                parrot.talk();
+        int[] arr = {5, 2, 4, 6, 1, 3};
+
+        insertionSort(arr);
+    }
+
+    static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int current = arr[i];
+            int j = i;
+            while (j > 0 && arr[j - 1] > current) {
+
             }
         }
     }
