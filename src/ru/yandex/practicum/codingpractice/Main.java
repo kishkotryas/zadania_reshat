7a4e8b6556adb68a1d20437e79a0806837ef79af
kishkotryas <scrath.thecountry.02@mail.ru>
2026-01-06 06:55:51 +0500

    feat: задание на сортировку слиянием

--- src/ru/yandex/practicum/codingpractice/Main.java
+++ src/ru/yandex/practicum/codingpractice/Main.java
@@ -1,20 +1,56 @@
 package ru.yandex.practicum.codingpractice;
 
 public class Main {
-    public static void main (String [] args) {
-        int[] arr = {5, 2, 4, 6, 1, 3};
+    public static void main(String[] args) {
+        int[] array1 = new int[]{1, 3, 4, 6, 8};
+        int[] array2 = new int[]{1, 2, 3, 8, 9};
+        int[] resultArray = merge(array1, array2);
 
-        insertionSort(arr);
-    }
-
-    static void insertionSort(int[] arr) {
-        for (int i = 1; i < arr.length; i++) {
-            int current = arr[i];
-            int j = i;
-            while (j > 0 && arr[j - 1] > current) {
-
-            }
+        for (int e : resultArray) {
+            System.out.print(e + ", ");
         }
     }
+
+    private static int[] merge(int[] leftArray, int[] rightArray) {
+
+        int leftSize = leftArray.length;
+        int rightSize = rightArray.length;
+        int[] resultArray = new int[leftSize + rightSize];
+
+        // Индексы, по которым идёт итерация
+        int left = 0;
+        int right = 0;
+        int index = 0;
+
+        while (index < resultArray.length) {
+
+            // Если левый индекс больше максимального левого индекса — добавляем элемент из правого подмассива.
+            if (left >= leftSize) {
+                resultArray[index] = rightArray[right];
+                right++;
+            }
+
+            // Если правый индекс больше максимального — добавляем элемент из левого подмассива.
+            else if (right >= rightSize) {
+                resultArray[index] = leftArray[left];
+                left++;
+            }
+
+            // Если оба текущих индекса внутри своих границ, нужно сравнивать элементы по этим индексам
+            // Если элемент в левом массиве меньше — добавляем его и увеличиваем левый индекс.
+            else if (leftArray[left] <= rightArray[right]) {
+                resultArray[index] = leftArray[left];
+                left++;
+            }
+            // Иначе — делаем тоже самое с правым индексом.
+            else {
+                resultArray[index] = rightArray[right];
+                right++;
+            }
+            index++;
+        }
+        return resultArray;
+    }
 }
+
 
