07c66479c48338452036bdb447c43dcc69d9166b
kishkotryas <scrath.thecountry.02@mail.ru>
2026-01-11 17:34:45 +0500

    проверка на ресет

--- src/ru/yandex/practicum/codingpractice/Main.java
+++ src/ru/yandex/practicum/codingpractice/Main.java
@@ -1,24 +1,18 @@
 package ru.yandex.practicum.codingpractice;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<Book> books = new ArrayList<>();
-        Comparator<Book> bookPageCountComparator = new BookPageCountComparator();
-        Comparator<Book> bookYearComparator = new BookYearComparator();
+        
+    }
+}
 
-        books.add(new Book("Чепчун", "Мася", 1488, Genre.HORROR, 92));
-        books.add(new Book("Апельсин", "Баська", 1590, Genre.DRAMA, 105));
-        books.add(new Book("Баськин домик", "Чепчик", 2025, Genre.ROMANTIC, 240));
-        books.add(new Book("Гойда", "Улитка", 690, Genre.THRILLER, 44));
-        System.out.println("ДО СОРТИРОВКИ:");
-        System.out.println(books);
-        System.out.println("СОРТИРОВКА ПО ЖАНРУ:");
-        books.sort(Comparator.comparing(Book::getGenre));
-        System.out.println(books);
+public static int islandSearch (int[][] matrix) {
+    int leftIndex = 0;
+    int rightIndex = 0;
+
+    for (int i = 0; i < ; i++) {
+        
     }
 }
