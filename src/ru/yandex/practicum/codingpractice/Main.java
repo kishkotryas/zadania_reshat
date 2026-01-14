294f8590c0e4d3c260d606ca32e15f152b1a4e1b
kishkotryas <scrath.thecountry.02@mail.ru>
2026-01-14 22:00:28 +0500

    feat: задание на реализации Set

--- src/ru/yandex/practicum/codingpractice/Main.java
+++ src/ru/yandex/practicum/codingpractice/Main.java
@@ -1,42 +1,28 @@
 package ru.yandex.practicum.codingpractice;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Main {
-    public static void main (String[] args) {
-        Map<String, LocalDateTime> historyOfOrders = new LinkedHashMap<>(16, 0_75, true);
-        addItem(historyOfOrders, "Футболка");
-        addItem(historyOfOrders, "Шорты");
-        addItem(historyOfOrders, "Носки");
-        addItem(historyOfOrders, "Ботинки");
-        addItem(historyOfOrders, "Кепка");
-        getHistory(historyOfOrders);
+    public static void main(String[] args) {
+        Set<Integer> hashNums = new HashSet<>();
+        Set<Integer> linkedHashNums = new LinkedHashSet<>();
+        Set<Integer> treeNums = new TreeSet<>();
+        addNums(hashNums);
+        addNums(linkedHashNums);
+        addNums(treeNums);
+        System.out.println(hashNums);
+        System.out.println(linkedHashNums);
+        System.out.print(treeNums);
     }
 
-    public static void getRecentViews(Map<String, LocalDateTime> items, int n) {
-        for (int i = 0; i <= n; i++) {
-            for (String item : items.keySet()) {
-                System.out.println(item);
-            }
-        }
-    }
-
-    public static void addItem(Map<String, LocalDateTime> items, String item) {
-        if (items.containsKey(item)) {
-            items.replace(item, LocalDateTime.now());
-        } else {
-            items.put(item, LocalDateTime.now());
-        }
-    }
-
-    public static void getHistory(Map<String, LocalDateTime> items) {
-        for (int i = 0; i < 3; i++) {
-            for (String item : items.keySet()) {
-                System.out.println(item + " = " + items.get(item));
-            }
-        }
+    public static void addNums (Set<Integer> nums) {
+        nums.add(5);
+        nums.add(3);
+        nums.add(1);
+        nums.add(4);
+        nums.add(2);
     }
 }
