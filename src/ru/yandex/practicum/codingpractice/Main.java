8fdff3d74d2848a5fba8afff9d8f7b1d7752efb4
kishkotryas <scrath.thecountry.02@mail.ru>
2026-02-02 03:13:20 +0500

    feat: задание на исключения

--- src/ru/yandex/practicum/codingpractice/Main.java
+++ src/ru/yandex/practicum/codingpractice/Main.java
@@ -3,14 +3,26 @@ package ru.yandex.practicum.codingpractice;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Set<String> animals = new HashSet<>();
-        List<String> animalsList = new ArrayList<>(List.of("dog", "bird", "duck", "cat"));
 
-        animals.add("cat");
-        animals.addAll(animalsList);
-        System.out.println(animals);
-        animals.remove("bird");
-        System.out.println("is bird in the hash set? " + animals.contains("bird"));
+    public static void main(String[] args) {
+        divideNums();
+    }
+
+    static void divideNums () {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.println("Введите первое число...");
+            int firstNum = scanner.nextInt();
+            System.out.println("Введите второе число...");
+            int secondNum = scanner.nextInt();
+            System.out.println(firstNum / secondNum);
+        } catch (ArithmeticException arithmetic) {
+            System.out.println("Ошибка: деление на ноль.");
+        } catch (InputMismatchException mismatch) {
+            System.out.println("Ошибка: введены не числовые значения.");
+        } catch (RuntimeException runtime) {
+            System.out.println("Произошла неопределенная ошибка!");
+        }
     }
 }
+
